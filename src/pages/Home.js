b00d51e23d60d0cde83457bf991d77bcb430f206
Sachin Kumar <mail.sachin161@gmail.com>
2023-07-15 00:24:26 +0530

    Home Page functional logic  implemented

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,62 +2,97 @@ import Navbar from "../components/Navbar";
 
 import avatar from "../assets/newdp.jpeg";
 
-import insta from "../assets/insta.jpg"
-import twitter from "../assets/twitter.jpg"
-import github from "../assets/github.png"
-import linkedin from "../assets/linkedin.png"
+import insta from "../assets/insta.jpg";
+import twitter from "../assets/twitter.jpg";
+import github from "../assets/github.png";
+import linkedin from "../assets/linkedin.png";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div id="home-main-div" >
-      <div
-        style={{
-          marginTop: 120,
-          display: "flex",
-          alignItems : "center",
-          flexDirection: "row",
-        }}
-      >
+      <div id="home-main-div">
         <div
           style={{
+            marginTop: 120,
             display: "flex",
-            flexDirection: "column",
+            alignItems: "center",
+            flexDirection: "row",
           }}
         >
           <div
             style={{
-              fontWeight: "bold",
-              fontSize: 50,
+              display: "flex",
+              flexDirection: "column",
             }}
           >
-            Hello 👋
+            <div
+              style={{
+                fontWeight: "bold",
+                fontSize: 50,
+              }}
+            >
+              Hello 👋
+            </div>
+            <p
+              style={{
+                fontWeight: "bold",
+                fontSize: 40,
+              }}
+            >
+              My name is Sachin Kumar <br /> I am a full stack developer.
+            </p>
+            <div>
+              <img
+                onClick={() => {
+                  window.open("https://www.linkedin.com/in/sachin161kr/");
+                }}
+                id="home-icons"
+                src={linkedin}
+              />
+              <img
+                onClick={() => {
+                  window.open("https://github.com/sachin161kr");
+                }}
+                id="home-icons"
+                src={github}
+              />
+              <img
+                onClick={() => {
+                  window.open("https://www.instagram.com/__sachinn___/");
+                }}
+                id="home-icons"
+                src={insta}
+              />
+              <img
+                onClick={() => {
+                  window.open("https://twitter.com/__sachinn___");
+                }}
+                id="home-icons"
+                src={twitter}
+              />
+            </div>
           </div>
-          <p
+          <div
             style={{
-              fontWeight: "bold",
-              fontSize: 40,
+              display: "flex",
+              flexDirection: "column",
+              marginLeft: 200,
             }}
           >
-            My name is Sachin Kumar <br /> I am a full stack developer.
-          </p>
-          <div>
-          <img id="home-icons"  src={linkedin} />
-          <img id="home-icons" src={github} />
-          <img id="home-icons"   src={insta} />
-          <img id="home-icons" src={twitter} />
-          </div> 
+            <img id="avatar" height={220} width={220} src={avatar} />
+            <button
+              onClick={() => {
+                window.open(
+                  "https://drive.google.com/file/d/13HfOH0nlIKR_PcZUmX-2fKJrDn0Lhoqx/view?usp=sharing"
+                );
+              }}
+              id="resume-btn"
+            >
+              Know more
+            </button>
+          </div>
         </div>
-        <div style={{
-            display : "flex",
-            flexDirection : "column",
-            marginLeft : 200,
-        }} >
-        <img id="avatar" height={220} width={220} src={avatar} />
-        <button id="resume-btn">Know more</button>
-        </div>
-      </div>
       </div>
     </>
   );
